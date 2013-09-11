8ef80001a7f3452d47bae813eaa5804e0f4619ad
SudyDuan <SudyDuan@gmail.com>
2013-09-11 19:59:55 +0800

    add some details

--- weibocn/weibocn/spiders/weibospider.py
+++ weibocn/weibocn/spiders/weibospider.py
@@ -9,13 +9,18 @@ from scrapy.item import Item
 class WeibocnSpider(CrawlSpider):
     name = 'weibo.cn'
     allowed_domains = ['weibo.cn']
-    start_urls = ['http://www.example.com']
+
+
+    start_urls = [
+					'http://weibo.cn/u/?vt=4',
+
+    ]
 
     rules = (
         # Extract links matching 'category.php' (but not matching 'subsection.php')
         # and follow links from them (since no callback means follow=True by default).
-        Rule(SgmlLinkExtractor(allow=('category\.php', ), deny=('subsection\.php', ))),
+        Rule(SgmlLinkExtractor(allow=('http://weibo\.cn/u/[0-9]{10}\?'),callback='parse')),
 
         # Extract links matching 'item.php' and parse them with the spider's method parse_item
-        Rule(SgmlLinkExtractor(allow=('item\.php', )), callback='parse_item'),
+        Rule(SgmlLinkExtractor(allow=('http://weibo\.cn/comment/', )), callback='parse_item'),
     )
